aed7d37cc41655198a4b4584ad70cfb08760bd4e
Luke <lukedesmondjames@gmail.com>
2025-03-04 16:50:44 +0000

    png-webp bug fix

--- src/components/home-page/hero/Hero.jsx
+++ src/components/home-page/hero/Hero.jsx
@@ -7,7 +7,7 @@ import SocialIcons from "../../global/SocialIcons"
  */
 const Hero = () => {
   return (
-    <div className="lg:bg-[url('./assets/dark-gradient.png')] md:bg-cover bg-dark-blue bg-right-top justify-center md:justify-start h-screen flex flex-col md:flex-row md:items-center md:my-8 md:py-16 md:px-16 md:mb-0 md:mt-0">
+    <div className="lg:bg-[url('./assets/dark-gradient.webp')] md:bg-cover bg-dark-blue bg-right-top justify-center md:justify-start h-screen flex flex-col md:flex-row md:items-center md:my-8 md:py-16 md:px-16 md:mb-0 md:mt-0">
       <div className="pb-0 md:pb-12 p-12 md:px-8 mb-4 md:mt-24 text-left text-white">
         <h1 className="text-accent text-5xl md:text-8xl">Luke James</h1>
         <h1 className="text-white font-thin text-3xl md:text-7xl md:border-none md:pb-0">Front-End Developer.</h1>
