a77a2fc68d8aee4d0e1a90a382788217f00a8b2f
Luke <lukedesmondjames@gmail.com>
2025-03-04 16:47:59 +0000

    png to webp convert

--- src/assets/index.jsx
+++ src/assets/index.jsx
@@ -1,8 +1,8 @@
-export { default as headshot } from './headshot.png';
-export { default as myChaiCover } from './my-chai-cover.png';
-export { default as plantBudCover } from './plant-bud-cover.png';
-export { default as moodBoardCover } from './mood-board.png';
+export { default as headshot } from './headshot.webp';
+export { default as myChaiCover } from './my-chai-cover.webp';
+export { default as plantBudCover } from './plant-bud-cover.webp';
+export { default as moodBoardCover } from './mood-board.webp';
 export { default as lightDarkMode } from './light-dark-mode.svg';
-export { default as portfolioCover } from './portfolio-cover.png';
-export { default as code } from './code.png';
+export { default as portfolioCover } from './portfolio-cover.webp';
+export { default as code } from './code.webp';
 export { default as cv } from './luke-james-cv.pdf';
